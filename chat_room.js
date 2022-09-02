db45eb3f3e62868218c3bbca9916dab8083b9306
sujeethakalimuthu <50768606+sujeethakalimuthu@users.noreply.github.com>
2022-09-02 16:53:07 +1000

    Update chat_room.js

---
+++ chat_room.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyB6p5waq6fXGTl-fhK3YV7QIYXZq3qkwSg",
+    authDomain: "chatapp-ba82c.firebaseapp.com",
+    databaseURL: "https://chatapp-ba82c-default-rtdb.firebaseio.com",
+    projectId: "chatapp-ba82c",
+    storageBucket: "chatapp-ba82c.appspot.com",
+    messagingSenderId: "1005639204521",
+    appId: "1:1005639204521:web:b9883c562ed6b91638f3b8",
+    measurementId: "G-8K6ZMPLQC0"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
